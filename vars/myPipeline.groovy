0519d6aecee8b651bc849315715797a953b47cce
naresh26git <111833906+naresh26git@users.noreply.github.com>
2024-05-06 15:44:19 +0530

    Update myPipeline.groovy

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -1,7 +1,7 @@
 def call() {
     pipeline {
         agent any
-        // stages {
+        stages {
         //     stage('Git Checkout') {
         //         steps {
         //             script {
