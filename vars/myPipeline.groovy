7f9ae5ab3937bd242f89da91ead50fa722641cf6
sharatharul2 <106569825+sharatharul2@users.noreply.github.com>
2025-07-23 22:35:50 +0530

    Update myPipeline.groovy

--- vars/myPipeline.groovy
+++ vars/myPipeline.groovy
@@ -5,7 +5,7 @@ def call() {
         //     stage('Git Checkout') {
         //         steps {
         //             script {
-        //                 git branch: 'node-dev', url: 'https://github.com/naresh26git/multi-branch.git' //project-repo
+        //                 git branch: 'node-dev', url: 'https://github.com/sharatharul2/multi-branch.git' //project-repo
         //             }
         //         }
         //     }
